2aa4719b0b7d93822e09b84ae036ca928c9bd104
linhle <linhle@Hieus-MacBook-Air.local>
2020-09-08 14:50:21 +0800

    add content

---
+++ src/components/Footer.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Footer() {
+    return (
+        <div>
+
+            <footer class="page-footer font-small grey lighten-3 py-4 dark-grey-text">
+
+                <div class="container">
+
+                    <div class="row">
+                        <div class="col-md-4">
+                            <img src="https://res.cloudinary.com/dt5rqi1l9/image/upload/v1599544924/logo_size_invert_zmyzml.jpg" height="30" alt="linh logo" />
+                        </div>
+                        <div class="col-md-4">
+                            <ul class="list-unstyled d-flex justify-content-center mb-0 mt-2">
+                                <Link to="/about">
+                                    <li>
+                                        <p class="mx-3">About</p>
+                                    </li>
+                                </Link>
+                                <Link to="/contact">
+                                    <li>
+                                        <p class="mx-3">Contact</p>
+                                    </li>
+                                </Link>
+                            </ul>
+                        </div>
+                        <div class="col-md-4">
+
+                            <div class="footer-copyright text-right bg-transparent dark-grey-text mt-2">© 2020.
+          <a class="dark-grey-text" href="https://mdbootstrap.com/education/bootstrap/"> Designed by Linh Le</a>
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+
+
+            </footer>
+
+        </div>
+    );
+}
+
+export default Footer;
---
+++ src/components/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import About from './About';
+import Education from './Education';
+import Skills from './Skills';
+import Projects from './Projects';
+import End from './End';
+import Cover from './Cover';
+
+function Home() {
+    return (
+        <div>
+            <Cover />
+            <About style_1="about" image="https://res.cloudinary.com/dt5rqi1l9/image/upload/v1599544701/pic1_qnqxeo.jpg"/>
+            <Education style_1="edu-work"/>
+            <Skills />
+            <Projects />
+            <End />
+        </div>
+    );
+}
+
+export default Home;
