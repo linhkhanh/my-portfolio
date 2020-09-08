4c67f31b4f1de329d4d7761aa5ad0c6a94f35a08
linhle <linhle@Hieus-MacBook-Air.local>
2020-09-08 14:32:18 +0800

    Updates

---
+++ precache-manifest.454f9943600ecdc4207ef87833a25a07.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2d0828521ab219f60351266a98e3ae1",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "08c4648d1040b3fec0b7",
+    "url": "/my-portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/my-portfolio/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-portfolio/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08c4648d1040b3fec0b7",
+    "url": "/my-portfolio/static/js/main.ecee3498.chunk.js"
+  },
+  {
+    "revision": "976294871add738ec1ea",
+    "url": "/my-portfolio/static/js/runtime-main.3134900d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
